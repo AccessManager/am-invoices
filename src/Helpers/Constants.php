751c91bd626eea727144e129e640a17892474a12
Rahul makhija <rahulmkhj@gmail.com>
2015-05-08 17:47:15 +0530

    added several new constants.

--- src/Helpers/Constants.php
+++ src/Helpers/Constants.php
@@ -1,1 +1,34 @@
 <?php
+if( ! defined('DAY'))
+	define('DAY', 1);
+
+if( ! defined('DAYS'))
+	define('DAYS', 1);
+
+if( ! defined('MONTH'))
+	define('MONTH', 30);
+
+if( ! defined('MONTHS'))
+	define('MONTHS', 30);
+
+if( ! defined('YEAR'))
+	define('YEAR', 365);
+
+if( ! defined('YEARS'))
+	define('YEARS', 365);
+
+if( ! defined('BILL_DURATION_CYCLE'))
+	define('BILL_DURATION_CYCLE', 1);
+
+if( ! defined('BILL_DURATION_FULL'))
+	define('BILL_DURATION_FULL', 2);
+
+if( ! defined('DISABLED'))
+	define('DISABLED', 0);
+
+if( ! defined('ENABLED'))
+	define('ENABLED', 1);
+if( ! defined('RECURRING'))
+	define('RECURRING', 1);
+if( ! defined('NON_RECURRING'))
+	define('NON_RECURRING', 0);
