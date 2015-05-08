f465c9c5a1023cc9c6a8ea4c2446e87461151ae5
Rahul makhija <rahulmkhj@gmail.com>
2015-05-08 17:47:37 +0530

    added 2 new helper methods.

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -1,1 +1,14 @@
 <?php
+
+function pr($array)
+{
+	echo "<pre>";
+	print_r($array);
+	echo "</pre>";
+	exit;
+}
+
+function isValidDate($date)
+{
+	return $date == '0000-00-00 00:00:00' || $date == NULL ? FALSE : TRUE;
+}
